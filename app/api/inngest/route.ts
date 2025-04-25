66eeb973f38ebd244b8360223a31256bbdf1dc48
sizan14789 <sizanalt@gmail.com>
2025-04-25 17:42:58 +0600

    "nothing

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,5 +1,5 @@
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/innjest";
 import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/innjest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
