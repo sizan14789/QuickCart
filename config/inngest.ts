2a45a575402505a7df2b7cacf9249c2a2ddf8eaf
sizan14789 <sizanalt@gmail.com>
2025-04-25 21:43:49 +0600

    'gh'

--- config/inngest.ts
+++ config/inngest.ts
@@ -12,7 +12,7 @@ export const syncUserCreation = inngest.createFunction(
     id: "sync-user-from-clerk",
   },
   {
-    event: "clerk/user.created",
+    event: 'clerk/user.created',
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
